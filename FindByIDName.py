32f8ff4ddd362f20bbb69675a1f1231334666b7a
JohnVonNeumann <louiswillcock@gmail.com>
2017-03-08 09:47:41 +1100

    committing here to mark a spot ive gotten too and explain an error i just sorted out. so basically i was defining setup incorrectly, turns up you have to name it setUp and not setup, the capitalisation is really important as you are using a unittest method and it's particular about implementation. so here i have managed to push a few bits and pieces out of the monolithic script and into their own methods in order to abstract things out and clean up the code. it's not running properly at this stage due to a few errors in my undersranding which ill be quick to fix. ive created two separate methods (other than setUp) to diff finding by name and id

--- FindByIDName.py
+++ FindByIDName.py
@@ -1,12 +1,17 @@
 from selenium import webdriver
+import unittest
 
-class FindByIdName():
+class FindByIdName(unittest.TestCase):
 
-    def test(self):
+    def setUp(self):
         baseUrl = "https://letskodeit.teachable.com/pages/practice"
         elementIdToFind = "name"
         elementNameToFind = "show-hide"
-        # var setting for easy change of inputs
+        self.driver = webdriver.Firefox()
+        self.driver.get(baseUrl)
+        self.addCleanup(self.driver.quit)
+
+    def testIdFind(self):
 
         driver = webdriver.Firefox()
         driver.get(baseUrl)
@@ -16,13 +21,13 @@ class FindByIdName():
             print("We found an element by the ID of " + elementIdToFind + ".")
             # if we can find our chosen elementById, print message
 
+    def testNameFind(self):
         elementByName = driver.find_element_by_name(elementNameToFind)
 
         if elementByName is not None:
             print("We found an element by the name of " + elementNameToFind + ".")
             # if we can find our chosen elementByName, print message
-        driver.quit()
-        # quit on completion
 
-ff = FindByIdName()
-ff.test()
+        # quit on completion
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
