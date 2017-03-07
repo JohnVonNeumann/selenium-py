44cab4896c49e0780e580663cd730978563278a2
JohnVonNeumann <louiswillcock@gmail.com>
2017-03-07 20:21:12 +1100

    touched up implementation and added a call to selenium to import webdriver, also added comments to explain it all to myself

--- unit_test.py
+++ unit_test.py
@@ -1,14 +1,24 @@
 import unittest
+from selenium import webdriver
+# standard lib we use for basic run unit test ttype functionality
 
 class GoogleTestCase(unittest.TestCase):
+# we create a new class and give it imported functionality provided by unittest lib
+# in this instance, because we creating a new test, we use TestCase, which is used
+# for small testcases.
+# Docs: https://docs.python.org/2/library/unittest.html
 
     def setUp(self):
         self.browser = webdriver.Chrome()
         self.addCleanup(self.browser.quit)
+        # addCleanup is used to clean up resources used by the test
 
     def testPageTitle(self):
         self.browser.get('http://www.google.com')
         self.assertIn('Google', self.browser.title)
 
 if __name__ == '__main__':
+    # first exposure to this type of test, so this gives us cli functionality in a clean way
+    #with formatting and whatnot.
     unittest.main(verbosity=2)
+    # main() can take a variety of args, verbosity=2 indicates we wish to see a lot of output info
