141ad1bf03447e1ad163fbaf0a7d14050afbcc5f
JohnVonNeumann <louiswillcock@gmail.com>
2017-03-08 10:24:45 +1100

    what will probably be the final addition to this script, added a failure message and some comments to highlight flow of the script

--- FindByIDName.py
+++ FindByIDName.py
@@ -3,27 +3,31 @@ import unittest
 
 class FindByIdName(unittest.TestCase):
 
-    def setUp(self):
+    def setUp(self): # setup and declaration of inputs required for sel and unittest to do its job
+        # --------- Edit inputs here: -------------
         baseUrl = "https://letskodeit.teachable.com/pages/practice"
         self.elementIdToFind = "name"
         self.elementNameToFind = "show-hide"
+        # --------- Do not edit below this line: -------------
         self.driver = webdriver.Firefox()
         self.driver.get(baseUrl)
 
-    def testIdFind(self):
+    def testIdFind(self): #simple find by Id test, outputs message on completion
         elementById = self.driver.find_element_by_id(self.elementIdToFind)
         if elementById is not None:
             print("We found an element by the ID of " + self.elementIdToFind + ".")
-            # if we can find our chosen elementById, print message
+        else:
+            print("Could not find an element by the ID of " + self.elementIdToFind + ".")
 
-    def testNameFind(self):
+    def testNameFind(self): # simple find by name test, outputs message on completion
         elementByName = self.driver.find_element_by_name(self.elementNameToFind)
         if elementByName is not None:
             print("We found an element by the name of " + self.elementNameToFind + ".")
-            # if we can find our chosen elementByName, print message
+        else:
+            print("Could not find an element by the name of " + self.elementNameToFind + ".")
 
-    def tearDown(self):
+    def tearDown(self): #cleanup resources and close windows when test completes
         self.addCleanup(self.driver.quit)
 
-if __name__ == "__main__":
+if __name__ == "__main__": # run cli output and functionality from unittest lib
     unittest.main(verbosity=2)
