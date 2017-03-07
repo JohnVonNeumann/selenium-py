46f5eef9391815594419eb356d36ca52a6a4fd7e
JohnVonNeumann <louiswillcock@gmail.com>
2017-03-08 10:02:07 +1100

    and she works as intended, i have no doubt that my implementation is probably ugly, and performance at the moment is umm, absolutely terrible (although in comparsion to the 120 second original times down to 94s, not bad). so the problem i was having here was that i wasnt using self properly, and im not sure if i am, even now. self appears to basically be instance variables from ruby, using the tag self instead of @, makes a lot of sense now. added some stuff in setup to cleanup resources once tests have completed and im happy that i was able to implement unitest by myself. next one will be even more interesting, ill start building scripts before watching the videos so i can correct errors and learn the content better.

--- FindByIDName.py
+++ FindByIDName.py
@@ -5,29 +5,26 @@ class FindByIdName(unittest.TestCase):
 
     def setUp(self):
         baseUrl = "https://letskodeit.teachable.com/pages/practice"
-        elementIdToFind = "name"
-        elementNameToFind = "show-hide"
+        self.elementIdToFind = "name"
+        self.elementNameToFind = "show-hide"
         self.driver = webdriver.Firefox()
         self.driver.get(baseUrl)
         self.addCleanup(self.driver.quit)
 
     def testIdFind(self):
 
-        driver = webdriver.Firefox()
-        driver.get(baseUrl)
-        elementById = driver.find_element_by_id(elementIdToFind)
+        elementById = self.driver.find_element_by_id(self.elementIdToFind)
 
         if elementById is not None:
-            print("We found an element by the ID of " + elementIdToFind + ".")
+            print("We found an element by the ID of " + self.elementIdToFind + ".")
             # if we can find our chosen elementById, print message
 
     def testNameFind(self):
-        elementByName = driver.find_element_by_name(elementNameToFind)
+        elementByName = self.driver.find_element_by_name(self.elementNameToFind)
 
         if elementByName is not None:
-            print("We found an element by the name of " + elementNameToFind + ".")
+            print("We found an element by the name of " + self.elementNameToFind + ".")
             # if we can find our chosen elementByName, print message
 
-        # quit on completion
 if __name__ == "__main__":
     unittest.main(verbosity=2)
