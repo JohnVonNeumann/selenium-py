b14cf7340d9aeb477cf20b06509518925fdb7206
JohnVonNeumann <louiswillcock@gmail.com>
2017-03-08 09:31:09 +1100

    working as needed with some small changes to pretty up the implementation, I'm also going to next make a commit to import some unitest functionality, this commit is fairly simple and ugly, we set inputs, then pull up a firefox window, look for elements by ID and Name and then quit the window after outputting messages to console. changes to come include dividing the script into logical parts and utilising unitest to turn it into a proper unit test with decent output, i think some refactoring can be done as well

--- FindByIDName.py
+++ FindByIDName.py
@@ -4,13 +4,25 @@ class FindByIdName():
 
     def test(self):
         baseUrl = "https://letskodeit.teachable.com/pages/practice"
-        elementToFind = "name"
+        elementIdToFind = "name"
+        elementNameToFind = "show-hide"
+        # var setting for easy change of inputs
+
         driver = webdriver.Firefox()
         driver.get(baseUrl)
-        elementById = driver.find_element_by_id(elementToFind)
+        elementById = driver.find_element_by_id(elementIdToFind)
 
         if elementById is not None:
-            print("We found #{elementToFind}")
+            print("We found an element by the ID of " + elementIdToFind + ".")
+            # if we can find our chosen elementById, print message
+
+        elementByName = driver.find_element_by_name(elementNameToFind)
+
+        if elementByName is not None:
+            print("We found an element by the name of " + elementNameToFind + ".")
+            # if we can find our chosen elementByName, print message
+        driver.quit()
+        # quit on completion
 
 ff = FindByIdName()
 ff.test()
--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Learning Selenium to help improve my test automation skills. Using Python instea
 * Been spending some time getting Firepath setup for using xpath, strange how they bundle the thing into firefox but then they don't support some basic addon functionality. Had to go back to firebug vxx.18 in order to get the xpath stuff working. Even then, might have to port back further seeing as the course i'm only recommended much older than .18
 
 * Now have webdrivers running for Chrome and Firefox, I'll build tests for those two. Anything more than that I can learn on the job when I come across them.
+
+* Created my first gitignore file with this project, was containing log files that weren't required to be publicly viewable.
