638f28997e85f68b741fb39da8b6e60141b27884
JohnVonNeumann <louiswillcock@gmail.com>
2017-03-08 13:31:45 +1100

    basic introduction to logging, i skipped some content as it was all pretty similar, ill cover the exacts as I get to them, i chose to move on and get to more advanced topics because i dont think i need to understand right now the exact differences between find by name and find by *insert here*, it's more important that im covering the more advanced stuff, i have enough knowledge to generalise when i hit different find-by methods. this commit is just an easy intro to logging output, interesting stuff actually seeing as this is how config log files are made, id always wondered and now I know. I will be going back and changing the original files i made in order to incorporate more advanced features and create a small example of a variety of different selenium features.

---
+++ logging_basics.py
@@ -0,0 +1,19 @@
+"""
+LOGGING LEVELS
+---------------
+DEBUG
+INFO
+WARNING
+ERROR
+CRITICAL
+
+These levels are ascending (descending here) in importance, also important to understand is that
+only WARNING+ will actually be output via terminal/to users.
+
+"""
+
+import logging
+
+logging.warning("This is a WARNING message.")
+logging.info("This INFO message will not output.")
+logging.error("This is an ERROR message.")
