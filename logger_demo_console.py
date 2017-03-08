48e8dbbb15cd91ee078937db138c40d040a06e31
JohnVonNeumann <louiswillcock@gmail.com>
2017-03-09 00:10:30 +1100

    removed a fair chunk of redundant code who's job has been relegated to the simple logging.conf file. output is no longer rendered to the terminal but to test.log, a log file held in current working directory, but i suppose you could place it anywhere, i would assume for production it would in fact be placed in the config directory

--- logger_demo_console.py
+++ logger_demo_console.py
@@ -9,21 +9,7 @@ class LoggerDemoConsole():
         #create logger
         logging.config.fileConfig('logging.conf') # allows us to fileConfig directly from a config file, like a Dockerfile build
         logger = logging.getLogger(LoggerDemoConsole.__name__) # ___name__ strips functionality off methods and turns them into strings
-        logger.setLevel(logging.INFO) # set the base level of logging
-
-        #create console handler and set level to info
-        chandler = logging.StreamHandler() #StreamHandler is one of many log handlers, in this case, allowing us to move logs to other places
-        chandler.setLevel(logging.INFO)
-
-        #create formatter
-        formatter = logging.Formatter('%(asctime)s -%(name)s - %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-
-        #add formatter to console handler -> ch
-        chandler.setFormatter(formatter)
-
-        #add console handler to logger
-        logger.addHandler(chandler)
-
+        
         #logging messages
         logger.debug('debug message')
         logger.info('info message')
