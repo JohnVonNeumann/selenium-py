0da52d34c5d3807787ac0a6453556486ce8806b9
JohnVonNeumann <louiswillcock@gmail.com>
2017-03-08 13:35:11 +1100

    getting off the train now so just gonna commit what i have so i dont lose my train (ha ha) of thought, only a small change to add in basicConfig logging to a test.log file inside the working dir. cool how it all works.

--- logging_basics.py
+++ logging_basics.py
@@ -14,6 +14,8 @@ only WARNING+ will actually be output via terminal/to users.
 
 import logging
 
+logging.basicConfig(filename="test.log", level=logging.DEBUG)
+# we set level as debug as it ensures ALL logging methods are recorded
 logging.warning("This is a WARNING message.")
 logging.info("This INFO message will not output.")
 logging.error("This is an ERROR message.")
