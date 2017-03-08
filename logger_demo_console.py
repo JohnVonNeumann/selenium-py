2415c30a50feee343c99e1cc354c1d0cf3f7a776
JohnVonNeumann <louiswillcock@gmail.com>
2017-03-08 23:28:49 +1100

    this is my attempt at implementing a logging console that creates an entire log handling pipeline, ive built this from the docsets and feel that ive done an ok job of it, ill have another look over it before i move onto the course's implementation, im also gonna add some comments to my attempt and adjust as needed. so here we import logger, set var logger using the lib and getLogger named sample_log. next we build the var chandler and set it to the StreamHandler class method, of which we then set log level to INFO. what this does, i think, is allows us to handle stream data, which we can push elsewhere, handler come in different shapes and sizes, there are also fileHandlers, which create and collate log data into log files, hence the name fileHandler. continuing, we format all streamHandler data to come out in a datatime readable format with clear messages.

--- logger_demo_console.py
+++ logger_demo_console.py
@@ -1,22 +1,26 @@
 import logging
 
 class LoggerDemoConsole():
+# https://docs.python.org/2/howto/logging-cookbook.html#multiple-handlers-and-formatters
 
     def testLog(self):
-        #create logger
 
+        #create logger
+        logger = logging.getLogger('sample_log')
+        logger.setLevel(logging.INFO)
 
         #create console handler and set level to info
-
+        chandler = logging,StreamHandler()
+        chandler.setLevel(logging.ERROR)
 
         #create formatter
-
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
         #add formatter to console handler -> ch
-
+        chandler.setFormatter(formatter)
 
         #add console handler to logger
-
+        logger.addHandler(chandler)
 
         #logging messages
         logger.debug('debug message')
