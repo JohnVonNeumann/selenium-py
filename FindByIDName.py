e76016e6d182f6dafeed652533ad0eea8407d431
JohnVonNeumann <louiswillcock@gmail.com>
2017-03-09 11:00:30 +1100

    i think i now have this figured out, got it running as intended i think, perhaps i was expecting something grander? maybe, not too sure though, so it's working as needed, it outputs to log file and builds itself, you could create whatever class or file you would like and it would create log files that overwrite for each test (which probably isnt what you want in all circumstances, as you would want some application runtime histories for some things. but yeah, you can now apply it across the board and it will work

--- FindByIDName.py
+++ FindByIDName.py
@@ -15,27 +15,27 @@ class FindByIdName(unittest.TestCase):
         # --------- Do not edit below this line: -------------
         self.driver = webdriver.Firefox()
         self.driver.get(baseUrl)
-        self.log
+        self.log.debug('Setup Complete')
 
     def testIdFind(self): #simple find by Id test, outputs message on completion
         elementById = self.driver.find_element_by_id(self.elementIdToFind)
         if elementById is not None:
-            print("We found an element by the ID of " + self.elementIdToFind + ".")
+            self.log.debug("We found an element by the ID of " + self.elementIdToFind + ".")
         else:
-            print("Could not find an element by the ID of " + self.elementIdToFind + ".")
-        self.log
+            self.log.error("Could not find an element by the ID of " + self.elementIdToFind + ".")
 
     def testNameFind(self): # simple find by name test, outputs message on completion
         elementByName = self.driver.find_element_by_name(self.elementNameToFind)
         if elementByName is not None:
-            print("We found an element by the name of " + self.elementNameToFind + ".")
+            self.log.debug("We found an element by the name of " + self.elementNameToFind + ".")
         else:
-            print("Could not find an element by the name of " + self.elementNameToFind + ".")
-        self.log
+            self.log.error("Could not find an element by the name of " + self.elementNameToFind + ".")
 
     def tearDown(self): #cleanup resources and close windows when test completes
         self.addCleanup(self.driver.quit)
-        self.log
+        self.log.debug('Teardown complete')
 
 if __name__ == "__main__": # run cli output and functionality from unittest lib
     unittest.main(verbosity=2)
+
+demo = FindByIdName()
