143f3da6cc323244a9b685922ec3fbcafc512183
JohnVonNeumann <louiswillcock@gmail.com>
2017-03-09 13:10:01 +1100

    set up basic boilerplate structure for test_suite script with class names, all imports that i believe I will need (this will probably increase as I go) and the final if statement to run the testsuite properly

--- kode_it_test_suite.py
+++ kode_it_test_suite.py
@@ -1,2 +1,29 @@
 # This is going to be a complete test suite that will run tests against all aspects of the Lets Kode It
 # webpage using Selenium-py
+
+from selenium import webdriver
+import unittest
+import logging
+import custom_logger as cl
+
+class KodeItTestSuite(unittest.TestCase):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
