65639a38334b606c83e5a8f7fb0ce637a44b1092
JohnVonNeumann <louiswillcock@gmail.com>
2017-03-13 23:20:13 +1100

    having issues atm with hidden elements, i initially f'd the implementation with the incorrect find_by method, but now im facing the wonders of fancy js, it appears that due to some things being required to have interaction in order to appear, it cant id it properly, also had to change how i was strucuting the file to get it to run, and i also realised that some of the elements that id gone through and done inital recon on, where actually different to the test run, because i searched for them via a smaller window, where bootstrap or whatever they use to make dynamic pages, provided html different to what was used when sel maximises the window

--- airbnb_test.py
+++ airbnb_test.py
@@ -6,40 +6,44 @@ from selenium.webdriver.common.keys import Keys
 baseUrl = "https://www.airbnb.com.au/"
 browser = webdriver.Firefox()
 browser.get(baseUrl)
+browser.maximize_window()
 
 #enter where you want to visit
 
     #initial search element
-    <span class="fakeInput_14hw9u3" data-reactid="107">
+    #<input id="search-location" class="LocationInput input-large" name="location" placeholder="Destination, city, address" autocomplete="off" value="" data-reactid="20" type="text">
+
+searchElement = browser.find_element_by_name('location')
+searchElement.send_keys('Melbourne, Victoria' + Keys.RETURN)
 
     #once inital has been clicked, modal opens up, where input
-    <input id="header-location--sm" class="input-large needsClick" placeholder="Destination, city, address" autocomplete="off" name="location" type="text"/>
+    #<input id="header-location--sm" class="input-large needsClick" placeholder="Destination, city, address" autocomplete="off" name="location" type="text"/>
 
 
 # enter checkin and checkout dates
 
     #when (checkin date picker)
-    <input id="header-checkin--sm" class="checkin input-large ui-datepicker-target" name="checkin" placeholder="Check in" type="text"/>
+    #<input id="header-checkin--sm" class="checkin input-large ui-datepicker-target" name="checkin" placeholder="Check in" type="text"/>
 
         # select checkin on the 16th
-        <a class="ui-state-default ui-state-hover" href="#">16</a>
+        #<a class="ui-state-default ui-state-hover" href="#">16#</a>
 
         # select checkout on the 18th
-        <a class="ui-state-default ui-state-hover" href="#">18</a>
+        #<a class="ui-state-default ui-state-hover" href="#">18#</a>
 
 
 # select number of guests from dropdown
 
     # select guests button
-    <button class="GuestPickerTrigger__button" type="button">
+    #<button class="GuestPickerTrigger__button" type="button">
 
     # select close
-    <button class="component_9w5i1l-o_O-component_button_r8o91c" type="button">
+    #<button class="component_9w5i1l-o_O-component_button_r8o91c" type="button">
 
 
 # click on search button
 
     # search button
-    <button class="btn btn-primary btn-large btn-block" type="submit">   Search </button>
+    #<button class="btn btn-primary btn-large btn-block" type="submit">   Search #</button>
 
 browser.quit()
