69dcad7b543c2a87159bb6a7c4fb29bd76a74697
JohnVonNeumann <louiswillcock@gmail.com>
2017-03-13 22:50:38 +1100

    basic setup of airbnb exercise file, in this commit, i have updated readme and started a small exercise to operate on airbnb's website and test my ability to select the date picker and city, then search for dates and whatnot. this file will be going through a few iterations, first will be a rough copy, before being refined into a cleaner version:

---
+++ airbnb_test.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+#open AirBnb.com
+baseUrl = "https://www.airbnb.com.au/"
+browser = webdriver.Firefox()
+browser.get(baseUrl)
+
+#enter where you want to visit
+
+
+# enter checkin and checkout dates
+
+
+# select number of guests from dropdown
+
+
+# click on search button
+
+
+
+browser.quit()
