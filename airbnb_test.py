dff03dc1a6f26ed3e86363968e447c8f35e0cc1b
JohnVonNeumann <louiswillcock@gmail.com>
2017-03-14 11:19:54 +1100

    probably should have done this over a few commits, pretty large, got on a bit of a roll with changes and fixes and next thing it was all pretty cleaned up, im still kinda wondering whether it's better to have it like this or if i should just do the whole thing as a script without classes and methods, im gonna do some reading on which is better. ive also been having a tonne of issues imporiting that blooody pyformance package, getting a module doesnt exist error repeatedly. this commit probably looks like ive gutted the whole thing, i wrote it in a separate file because i was trying to see if the error (pyfromance) was venv related or what, doesnt seem to be because i made a new one just for it and still got no love from it.

--- airbnb_test.py
+++ airbnb_test.py
@@ -1,38 +1,46 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from pyformance.registry
 
 
-#open AirBnb.com
-baseUrl = "https://www.airbnb.com.au/"
-browser = webdriver.Firefox()
-browser.get(baseUrl)
-browser.maximize_window()
+class AirbnbTestSuite():
+
+    def setup(self):
+        #open AirBnb.com
+        baseUrl = "https://www.airbnb.com.au/"
+        self.browser = webdriver.Firefox()
+        self.browser.get(baseUrl)
+        self.browser.maximize_window()
+
+    def searchLocation(self):
+        #enter where you want to visit
+        searchElement = self.browser.find_element_by_id('search-location')
+        searchElement.send_keys('Melbourne, Victoria' + Keys.RETURN)
+
+    def checkinout(self):
+        # enter checkin and checkout dates
+        startDateElement = self.browser.find_element_by_id('startDate').click()
+        arrivalDateToSelect = self.browser.find_element_by_xpath("html/body/main/div/div/div[2]/div[1]/div[1]/div/form/div/div/div[2]/div/div/div[2]/div/div/div[3]/div/div[2]/table/tbody/tr[3]/td[3]").click()
+        departureDateToSelect = self.browser.find_element_by_xpath("html/body/main/div/div/div[2]/div[1]/div[1]/div/form/div/div/div[2]/div/div/div[2]/div/div/div[3]/div/div[2]/table/tbody/tr[3]/td[7]").click()
+
+    def selectGuests(self):
+        # select number of guests from dropdown
+        guestClickElement = self.browser.find_element_by_xpath("html/body/main/div/div/div[2]/div[1]/div[1]/div/form/div/div/div[3]/div/button").click()
+        addOneAdultClick = self.browser.find_element_by_xpath("html/body/main/div/div/div[2]/div[1]/div[1]/div/form/div/div/div[3]/div/div/div/div/div[1]/div/div[2]/div/button").click()
+        closeAddGuest = self.browser.find_element_by_xpath("html/body/main/div/div/div[2]/div[1]/div[1]/div/form/div/div/div[3]/div/div/div/div/div[4]/div/div[2]/div/button").click()
+
+    def completeSearch(self):
+        # click on search button
+        clickSearch = self.browser.find_element_by_xpath("html/body/main/div/div/div[2]/div[1]/div[1]/div/form/div/div/div[4]/button").click()
 
 
-#enter where you want to visit
-searchElement = browser.find_element_by_id('search-location')
-searchElement.send_keys('Melbourne, Victoria' + Keys.RETURN)
+    #exit the script and close the window
+    def finishTest(self):
+        self.browser.quit()
 
-
-# enter checkin and checkout dates
-startDateElement = browser.find_element_by_id('startDate').click()
-arrivalDateToSelect = browser.find_element_by_xpath("html/body/main/div/div/div[2]/div[1]/div[1]/div/form/div/div/div[2]/div/div/div[2]/div/div/div[3]/div/div[2]/table/tbody/tr[3]/td[3]").click()
-departureDateToSelect = browser.find_element_by_xpath("html/body/main/div/div/div[2]/div[1]/div[1]/div/form/div/div/div[2]/div/div/div[2]/div/div/div[3]/div/div[2]/table/tbody/tr[3]/td[7]").click()
-browser.implicitly_wait(50)
-
-
-# select number of guests from dropdown
-guestClickElement = browser.find_element_by_xpath("html/body/main/div/div/div[2]/div[1]/div[1]/div/form/div/div/div[3]/div/button").click()
-addOneAdultClick = browser.find_element_by_xpath("html/body/main/div/div/div[2]/div[1]/div[1]/div/form/div/div/div[3]/div/div/div/div/div[1]/div/div[2]/div/button").click()
-closeAddGuest = browser.find_element_by_xpath("html/body/main/div/div/div[2]/div[1]/div[1]/div/form/div/div/div[3]/div/div/div/div/div[4]/div/div[2]/div/button").click()
-browser.implicitly_wait(50)
-
-
-# click on search button
-clickSearch = browser.find_element_by_xpath("html/body/main/div/div/div[2]/div[1]/div[1]/div/form/div/div/div[4]/button").click()
-browser.implicitly_wait(50)
-
-
-#exit the script and close the window
-browser.quit()
+ff = AirbnbTestSuite()
+ff.setup()
+ff.searchLocation()
+ff.checkinout()
+ff.selectGuests()
+ff.completeSearch()
+ff.finishTest()
